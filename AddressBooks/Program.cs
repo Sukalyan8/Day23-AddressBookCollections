c79d91b030809da407315123e85c6c37263511f4
Sukalyan <sukalyandash1920@gmail.com>
2022-01-31 14:14:02 +0530

    Add New Contact

---
+++ AddressBooks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book System!");
+            AddressBook.GetCustomer();
+            AddressBook.ListingPeople();
+        }
+    }
+}
